352eda256a9b34bc9d785e335ecef066d0ac17fd
nightshadow92 <41315829+nightshadow92@users.noreply.github.com>
2023-04-08 00:17:00 -0500

    Create irc-down.js

---
+++ irc-down.js
@@ -0,0 +1,32 @@
+const XDCC = require('xdccjs').default
+const fs = require('fs');
+
+
+let opts = {
+    host: 'irc.rizon.net', // IRC hostname                                                   - required
+    port: 6667, // IRC port                                                                   - default: 6667
+    retry: 5, // Nb of retries before skip                                                    - default: 1
+    timeout: 20, // Nb of seconds before a download is considered timed out                   - default: 30
+    verbose: true, // Display download progress and jobs status                               - default: false
+    botNameMatch: false,
+    path: process.cwd()
+}
+const xdccclient = new XDCC(opts)
+
+xdccclient.on('ready', async () => {
+    fs.readFile(process.cwd()+'/downlist.txt', 'utf8', (err, data) => {
+        data.split(/\r?\n/).forEach(item => {
+            xdccclient.download('Ginpachi-Sensei', [item])
+        });
+    });
+
+    xdccclient.on('downloaded', fileInfo => {
+        console.log(fileInfo.filePath)
+    })
+    xdccclient.on('done', job => {
+        console.log(job)
+    })
+    xdccclient.on('can-quit', () => {
+        xdccclient.quit()
+    })
+})
